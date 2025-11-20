492152b12567975d0672682e4f25adbc56bfb4d7
Ombuweb <tjiheron@gmail.com>
2025-11-20 22:28:03 +0100

    (web) clean up

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -2,18 +2,13 @@
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-
-  extends: [
-    '~/layers/01-base',
-    '~/layers/auth',
-    '~/layers/detection',
-    '~/layers/payment',
-    '~/layers/error-handling',
-  ],
-
+  ignore: ['/studio/node_modules'],
+  experimental: {
+    appManifest: false,
+  },
   // devtools: { enabled: true },
   vite: {
-    server: { watch: { usePolling: true, interval: 150 } },
+    // server: { watch: { usePolling: true, interval: 150 } },
   },
 
   modules: ['@nuxt/test-utils/module'],
